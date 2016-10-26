2d615e5e5cef02bbb49237bf7483a772a5d565c1
Evan <daley.evan@gmail.com>
2016-10-26 08:51:36 -0700

    Add view for displaying 3d objects

---
+++ 2016-10-25-CardboardVR5/Assets/VR/DynamicMenu/CreateMenuOptionTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateMenuOptionTarget : LookTarget 
+{
+	public CreateMenuOption createMenuOption;
+
+	public override  void FireEvent()
+	{
+		MenuController.Instance.PerformCreateMenuAction (createMenuOption);
+	}
+}
+
+public enum CreateMenuOption
+{
+	next,
+	prev
+}
